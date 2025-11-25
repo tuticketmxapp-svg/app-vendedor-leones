e768ec490211ad6bae1652b6316b1a6e8884a76e
Yazmin Fuentes <yazmin_1808@hotmail.com>
2025-11-25 07:03:26 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-vendedor-leones',
+  webDir: 'www'
+};
+
+export default config;
